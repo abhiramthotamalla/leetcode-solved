f0914d90dffa05396699ab63b5d263a3f71bc627
abhiramthotamalla <140891185+abhiramthotamalla@users.noreply.github.com>
2023-08-15 12:36:02 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.py
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.py
@@ -3,23 +3,19 @@
 class Solution:
     def equalPartition(self, N, arr):
         # code here
-        def isSubsetSum ( N, arr, sum):
-            dp=[[False for i in range(sum+1)] for _ in range(N+1)]
-            for i in range(N+1):
-                dp[i][0]=True
-            for i in range(1,N+1):
-                for j in range(1,sum+1):
-                    if(arr[i-1]<=j):
-                        dp[i][j]=(dp[i-1][j-arr[i-1]] or dp[i-1][j])
-                    else:
-                        dp[i][j]=dp[i-1][j]
-            
-            return dp[N][sum]
-            
-        if(sum(arr)%2):
-            return False
-        return isSubsetSum(N,arr,sum(arr)//2)
-        
+        if sum(arr)%2:
+            return 0
+        sum1=sum(arr)//2
+        dp=[[False for i in range(sum1+1)] for j in range(N+1)]
+        for i in range(N+1):
+            dp[i][0]=True
+        for i in range(1,N+1):
+            for j in range(1,sum1+1):
+                if(arr[i-1]<=j):
+                    dp[i][j]=(dp[i-1][j-arr[i-1]] or dp[i-1][j])
+                else:
+                    dp[i][j]=dp[i-1][j]
+        return dp[N][sum1]
 
 
 #{ 
