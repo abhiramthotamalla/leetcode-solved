6a4364e383d919ad81edf16ca3c665a3e624927d
abhiramthotamalla <140891185+abhiramthotamalla@users.noreply.github.com>
2023-09-13 19:05:06 +0530

    Time: 41 ms (76.51%), Space: 16.2 MB (73.95%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,18 @@
+import decimal
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        
+
+        # Set the precision of the decimal module to a suitable value
+        
+
+        # Input numerical strings
+        
+
+        # Perform the multiplication
+        result = int(num1) * int(num2)
+
+        # Convert the result back to a numerical string
+        result_str = str(result)
+
+        return result_str
