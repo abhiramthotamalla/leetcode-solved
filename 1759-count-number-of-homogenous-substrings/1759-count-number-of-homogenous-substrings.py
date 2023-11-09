1f80a6c0c791a487aed8d857a7716ba35fd603f3
abhiramthotamalla <140891185+abhiramthotamalla@users.noreply.github.com>
2023-11-10 00:23:08 +0530

    Time: 86 ms (96.40%), Space: 17.6 MB (18.35%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.py
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.py
@@ -1,27 +1,8 @@
 class Solution:
     def countHomogenous(self, s: str) -> int:
-        d=dict()
-        unique=dict()
-        i=0
+        mod=(10**9)+7
         res=0
-        while i<len(s):
-            dum=s[i]
-            c=s[i]
-            if c not in unique:
-                unique[c]=0
-            while(i<len(s)-1 and c==s[i+1]):
-                dum+=c
-                i+=1
-            d[dum]=d.get(dum,0)+1
-            i+=1
-        # print(d)
-
-        for c in d:
-            dum=c[0]
-            n=len(c)
-            count=(n*(n+1))//2
-            res+=count*d[c]
-            # print(res)
-            
-            
-        return res%((10**9)+7)
+        for i,j in groupby(s):
+            n=len(list(j))
+            res+=(n*(n+1))//2
+        return res%mod
