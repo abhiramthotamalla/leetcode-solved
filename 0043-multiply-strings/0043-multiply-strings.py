97abe7ab6ccd7cf81432b9abd51fbb8266cb96ef
abhiramthotamalla <140891185+abhiramthotamalla@users.noreply.github.com>
2023-11-12 16:02:31 +0530

    Time: 138 ms (17.06%), Space: 16.2 MB (46.72%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,17 +1,18 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        
-
-        # Set the precision of the decimal module to a suitable value
-        
-
-        # Input numerical strings
-        
-
-        # Perform the multiplication
-        result = int(num1) * int(num2)
-
-        # Convert the result back to a numerical string
-        result_str = str(result)
-
-        return result_str
+        if "0" in [num1,num2]:
+            return "0"
+        resl=len(num1)+len(num2)
+        res=[0]*resl
+        num1,num2=num1[::-1],num2[::-1]
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                digit = int(num1[i])*int(num2[j])
+                res[i+j]+=digit
+                res[i+j+1]+=res[i+j]//10
+                res[i+j]=res[i+j]%10
+        i=-1
+        while(res[i]==0):
+            res.pop()
+        res=map(str,res[::-1])
+        return "".join(res)
