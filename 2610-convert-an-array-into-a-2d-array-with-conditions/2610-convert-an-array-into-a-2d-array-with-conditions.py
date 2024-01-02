284ebce15d904b9fceda1bc7ebc75181ccdb9911
abhiramthotamalla <140891185+abhiramthotamalla@users.noreply.github.com>
2024-01-03 00:33:50 +0530

    Time: 79 ms (5.01%), Space: 17.6 MB (5.17%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -1,10 +1,5 @@
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
-        d=dict()
-        for i in nums:
-            d[i]=1+d.get(i,0)
-        k1=d.values()
-        
         nums.sort()
         maxi=0
         i=0
@@ -22,16 +17,31 @@ class Solution:
             i=j
             
         print(maxi)
-        print(max(k1))
         
         res=[[] for i in range(maxi+1)]
         
-        for v in d:
+        # for v in d:
+        #     c=0
+        #     k=d[v]
+        #     while(k and c<max(k1)):
+        #         if v not in res[c]:
+        #             res[c].append(v)
+        #             k-=1
+        #         c+=1
+        
+        i=0
+        while(i<len(nums)):
+            j=i+1
             c=0
-            k=d[v]
-            while(k and c<max(k1)):
-                if v not in res[c]:
-                    res[c].append(v)
-                    k-=1
-                c+=1
+            res[c].append(nums[i])
+            c+=1
+            while(j<len(nums)):
+                if nums[i]==nums[j]:
+                    res[c].append(nums[i])
+                    c+=1
+                    j+=1
+                else:
+                    break
+            i=j
+                
         return res
