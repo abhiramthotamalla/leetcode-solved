de11a48566ac855d544279a98099ea7d5822be6d
abhiramthotamalla <140891185+abhiramthotamalla@users.noreply.github.com>
2024-03-01 10:43:34 +0530

    Time: 34 ms (81.25%), Space: 16.5 MB (75.75%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.py
@@ -1,15 +1,12 @@
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
-        res=["1"]
-        one,zero=-1,0
-        for i in s:
-            if i=="1":
-                one+=1
-            else:
-                zero+=1
-        for i in range(zero):
-            res.append("0")
-        for i in range(one):
-            res.append("1")
-        res=res[::-1]
-        return "".join(res)
+        s=list(s)
+        s.sort()
+        i=0
+        while(i<len(s)):
+            if s[i]=="1":
+                break
+            i+=1
+        s[i],s[0]=s[0],s[i]
+        s=s[::-1]
+        return "".join(s)
